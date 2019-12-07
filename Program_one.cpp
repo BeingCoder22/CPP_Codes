c640ca61f70429962b2e1f0e837061e3547cfdbf
CodingSince14 <almightyshivam13396@gmail.com>
2019-12-07 14:11:46 +0530

    ok ok

--- Program_one.cpp
+++ Program_one.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     cout<<"i love programming"<<endl;
-    cout<<"Pusing into git";
+    cout<<"Pusing into git"<<endl;
+    cout<<"Something has happned";
     return 0;
 }
