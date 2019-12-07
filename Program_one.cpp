c7168e5a948fd53306efc4fd5af0e2dd99011a01
BeingCoder22 <vermaji229@gmail.com>
2019-12-07 14:16:39 +0530

    Ok fine

--- Program_one.cpp
+++ Program_one.cpp
@@ -5,6 +5,7 @@ int main()
 {
     cout<<"i love programming"<<endl;
     cout<<"Pusing into git"<<endl;
-    cout<<"Something has happned";
+    cout<<"Something has happned"<<endl;
+    cout<<"Changed owner";
     return 0;
 }
